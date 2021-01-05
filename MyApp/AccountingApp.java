3c0f8e35edbfc42a1111d429ee6b8e8daaa844d7
ChanhyukPark-Tech <pgg6713@gmail.com>
2021-01-05 20:22:04 +0900

    argument

--- MyApp/AccountingApp.java
+++ MyApp/AccountingApp.java
@@ -1,17 +1,29 @@
+import javax.swing.JOptionPane;
 
 public class AccountingApp {
 
 	public static void main(String[] args) {
-		System.out.println("Value of supply : "+12345.0);
-		System.out.println("VAT" + (12345.0*0.1) );
-		System.out.println("Total : " + (12345.0 + 12345.0*0.1) );
-		System.out.println("Income : " + (12345.0 - 12345.0*0.3));
-		System.out.println("Dividend 1 : " + (12345.0 - 12345.0*0.3) * 0.5 );
-		System.out.println("Dividend 1 : " + (12345.0 - 12345.0*0.3) * 0.3 );
-		System.out.println("Expense : " + (12345.0*0.3));
-		System.out.println("Dividend 1 : " + (12345.0 - 12345.0*0.3) * 0.2 );
+	
 		
+		double ValueOfSupply = Double.parseDouble(args[0]);
+		double expenseRate = 0.3;
+		double VAT = 0.1;
+		double vat = ValueOfSupply*VAT;
+		double total = ValueOfSupply + vat;
+		double income = ValueOfSupply - ValueOfSupply*expenseRate;
+		double dividend1 = income * 0.5;
+		double dividend2 = income * 0.3;
+		double dividend3 = income * 0.2;
 		
+		System.out.println("Value of supply : " + ValueOfSupply);
+		System.out.println("VAT : " + vat );
+		System.out.println("Total : " + total );
+		System.out.println("Income : " + income);
+		System.out.println("Dividend 1 : " + dividend1 );
+		System.out.println("Dividend 1 : " + dividend2 );
+		System.out.println("Expense : " + (ValueOfSupply*expenseRate));
+		System.out.println("Dividend 1 : " + dividend3 );
+
 	}
 
 }
