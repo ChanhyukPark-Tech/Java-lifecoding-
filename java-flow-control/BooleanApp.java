70b1be8fe8dbc930cde6badb4dcd933c25351b7f
ChanhyukPark-Tech <69495129+ChanhyukPark-Tech@users.noreply.github.com>
2021-01-07 02:10:24 +0900

    java - flow - control
    if / boolean / comparisonoperator /

---
+++ java-flow-control/BooleanApp.java
@@ -0,0 +1,21 @@
+
+public class BooleanApp {
+
+	public static void main(String[] args) {
+		System.out.println("One");
+		System.out.println(1);
+		
+		System.out.println(true);
+		System.out.println(false);
+		
+		String foo = "Hello World";
+//		String true = "Hello World"; reserved word 
+		
+		System.out.println(foo.contains("Hello"));
+		System.out.println(foo.contains("hello"));
+		System.out.println(foo.contains("o W"));
+		System.out.println(foo.contains("chanhyuk"));
+		
+	}
+
+}
